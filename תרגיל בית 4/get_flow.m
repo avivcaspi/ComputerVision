8a21822156a4452c5ed066b3d5b66eb8f591c274
avivcaspi <avivcaspi@gmail.com>
2021-01-23 11:12:26 +0200

    finish work

--- תרגיל בית 4/get_flow.m
+++ תרגיל בית 4/get_flow.m
@@ -1,9 +1,12 @@
 function [flow] = get_flow(frames,N,t)
+% Calculate the derivative dx dy by convoution using [1 -1] filter
 dx = [1 -1];
 dy = dx';
 frame_num = size(frames,4);
 frames_dx = convn(frames(:,:,1,:), dx, "same");
 frames_dy = convn(frames(:,:,1,:), dy, "same");
+
+% Calculate the derivative dt by substructing the frames 
 frames_dt = frames(:,:,1,2:frame_num) - frames(:,:,1,1:frame_num-1);
 
 row_num = size(frames,1);
@@ -15,16 +18,21 @@ flow = zeros(num_of_regions,num_of_regions,2,frame_num - 1);
 for i=1:frame_num-1
     for row=1:num_of_regions
         for col=1:num_of_regions
+            % get the pixel that belongs to the region (row,col)
             reg_dx_t = frames_dx((row-1)*N + 1:row*N,(col-1)*N + 1:col*N,:,i)';
             reg_dy_t = frames_dy((row-1)*N + 1:row*N,(col-1)*N + 1:col*N,:,i)';
+            % build the A matrix using the derivatives
             A = [reg_dx_t(:)'; reg_dy_t(:)']';
             AtA = A' * A;
             
+            % build the vector b
             reg_dt_t = frames_dt((row-1)*N + 1:row*N,(col-1)*N + 1:col*N,:,i)';
             b = -reg_dt_t(:);
             
+            % eigen values of AtA
             e = eig(AtA);
             if min(e) > t
+                % saving the flow vector of each region
                 u = AtA \ (A' * b);
                 flow(row,col,:,i) = u;
             end
--- תרגיל בית 4/show_flow.m
+++ תרגיל בית 4/show_flow.m
@@ -1,15 +1,18 @@
 function  x = show_flow(frames,flow, N, t)
+% show the arrows in the middel of each region
 [x,y] = meshgrid(N/2:N:512);
 filename = ['result_N_' , num2str(N) , '_t_' , num2str(t) , '.gif'];
 h = figure;
 axis tight manual
 for i=1:size(frames,4)-1
+    % show the flow on top of the frame
     imshow(frames(:,:,:,i), []);
     hold on
     quiver(x,y, flow(:,:,1,i), flow(:,:,2,i));
     hold off
     title(['N=' , num2str(N) , ', t=' , num2str(t)]);
-
+    
+    % Save the frames as gif 
     frame = getframe(h); 
     im = frame2im(frame); 
     [imind,cm] = rgb2ind(im,256);
